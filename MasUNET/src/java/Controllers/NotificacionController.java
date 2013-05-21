158d1a5b00c85cc80eac4e7d5c6b564f22933df5
jesus.lopezlugo <jesus.lopezlugo@b7059452-56c6-6192-a83b-376c0eeb31d7>
2013-05-21 06:25:40 +0000

    *Cambios en el Bundle para agregar las etiquetas del modulo de Configuracion. * Update de la ultima conexion y status del usuario online cuando se incia y cierra sesion

--- MasUNET/src/java/Controllers/NotificacionController.java
+++ MasUNET/src/java/Controllers/NotificacionController.java
@@ -4,9 +4,9 @@ import Entities.Notificacion;
 import Facades.NotificacionFacade;
 import java.io.Serializable;
 import javax.annotation.PostConstruct;
-import javax.inject.Named;
-import javax.inject.Inject;
 import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
 
 @Named(value = "notificacionController")
 @SessionScoped
